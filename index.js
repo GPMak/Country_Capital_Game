f4de38ffc66caf4c355d0058bd9eae8839f6645f
Galyna Prymak <galynaprymak@Galynas-MacBook-Pro.local>
2023-08-19 16:33:18 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,80 @@
+// STEPS:
+// 1. Import express and axios
+
+// 2. Create an express app and set the port number.
+
+// 3. Use the public folder for static files.
+
+// 4. When the user goes to the home page it should render the index.ejs file.
+
+// 5. Listen on your predefined port and start the server.
+
+// 6. Use axios to get a random country name and pass it to index.ejs to display the
+// it and the submit field to enter the capital of that country:
+// 6.1. Create an array with country codes numbers, to be used for selecting a random country.
+// 6.2. Create a button, which when clicked a random country name and a submit field appear.
+// 6.3. Create a code that shows either CORRECT or INCORRECT when a capital is entered in the submit field.
+
+// 7. Update CSS file.
+
+import express, { response } from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+const API_URL = "https://restcountries.com/v3.1/alpha?codes=";
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+})
+
+let capital = "";
+let name = "";
+let flag = "";
+
+app.post("/submit", async (req, res) => {
+    try {
+      const countryCodes = ["ABW", "AFG", "AGO", "AIA", "ALA", "ALB", "AND", "ARE", "ARG", "ARM", "ASM", "ATA", "ATF", "ATG", "AUS", "AUT", "AZE", "BDI", "BEL", "BEN", "BES", "BFA", "BGD", "BGR", "BHR", "BHS", "BIH", "BLM", "BLR", "BLZ", "BMU", "BOL", "BRA", "BRB", "BRN", "BTN", "BVT", "BWA", "CAF", "CAN", "CCK", "CHE", "CHL", "CHN", "CIV", "CMR", "COD", "COG", "COK", "COL", "COM", "CPV", "CRI", "CUB", "CUW", "CXR", "CYM", "CYP", "CZE", "DEU", "DJI", "DMA", "DNK", "DOM", "DZA", "ECU", "EGY", "ERI", "ESH", "ESP", "EST", "ETH", "FIN", "FJI", "FLK", "FRA", "FRO", "FSM", "GAB", "GBR", "GEO", "GGY", "GHA", "GIB", "GIN", "GLP", "GMB", "GNB", "GNQ", "GRC", "GRD", "GRL", "GTM", "GUF", "GUM", "GUY", "HKG", "HMD", "HND", "HRV", "HTI", "HUN", "IDN", "IMN", "IND", "IOT", "IRL", "IRN", "IRQ", "ISL", "ISR", "ITA", "JAM", "JEY", "JOR", "JPN", "KAZ", "KEN", "KGZ", "KHM", "KIR", "KNA", "KOR", "KWT", "LAO", "LBR", "LBY", "LCA", "LIE", "LKA", "LSO", "LTU", "LUX", "LVA", "MAC", "MAF", "MAR", "MCO", "MDA", "MDG", "MDV", "MEX", "MHL", "MKD", "MLI", "MLT", "MMR", "MNE", "MNG", "MNP", "MOZ", "MRT", "MSR", "MTQ", "MUS", "MWI", "MYS", "MYT", "NAM", "NCL", "NER", "NFK", "NGA", "NIC", "NIU", "NLD", "NOR", "NPL", "NRU", "NZL", "OMN", "PAK", "PAN", "PCN", "PER", "PHL", "PLW", "PNG", "POL", "PRI", "PRK", "PRY", "PSE", "PYF", "QAT", "REU", "ROU", "RUS", "RWA", "SAU", "SDN", "SEN", "SGP", "SGS", "SHN", "SJM", "SLB", "SLE", "SLV", "SMR", "SOM", "SPM", "SRB", "SSD", "STP", "SUR", "SVK", "SVN", "SWE", "SWZ", "SXM", "SYC", "SYR", "TCA", "TCD", "TGO", "THA", "TJK", "TKL", "TKM", "TLS", "TON", "TTO", "TUN", "TUR", "TUV", "TWN", "TZA", "UGA", "UKR", "UMI", "URY", "USA", "UZB", "VAT", "VCT", "VEN", "VGB", "VIR", "VNM", "VUT", "WLF", "WSM", "YEM", "ZAF", "ZMB", "ZWE"];
+      const randomCountry = countryCodes[Math.floor(Math.random()*countryCodes.length)];
+      const response = await axios.get("https://restcountries.com/v3.1/alpha?codes="+randomCountry);
+      const result = response.data; 
+     
+      res.render("index.ejs", {content: result});
+      capital = result[0].capital;
+      name = result[0].name.common;
+      flag = result[0].flags.svg;
+      return capital;
+    } catch (error) {
+      console.error("Failed to make request:", error.message);
+      res.render("index.ejs", {
+        error: error.message,
+      });
+    }
+  });
+
+  app.post("/submitCapital", (req, res) => {
+    const capitalCheck = {
+      submitted: req.body.capital,
+      database: capital[0],
+      name: name,
+      flag: flag
+    }
+    res.render("index.ejs", {game: capitalCheck});
+    console.log(capitalCheck.submitted===capitalCheck.database);
+    console.log(capitalCheck.submitted);
+    console.log(capitalCheck.database);
+    
+  })
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+
+
+
+
+
